0525c6b313c698785216810173eb237cb63991cd
KEN-MOITRA <ken.moitra@gmail.com>
2025-04-07 21:00:32 +0530

    maindecoder.v

--- maindecoder.v
+++ maindecoder.v
@@ -18,12 +18,12 @@ assign{mem_to_reg,mem_write,alu_src,reg_dst,reg_write,branch,jump}=control_signa
 
 always@(*)
 case(instruct[31:26])
-6'b000000:control_signal<=9'b0001100;// R-type
-6'b100011:control_signal<=9'b1010100;//LW
-6'b101011:control_signal<=9'b0110000;//SW
-6'b000100:control_signal<=9'b0000010;//BEQ
-6'b001000:control_signal<=9'b0010100;//ADDI
-6'b000010:control_signal<=9'b0000001;//J
-default:control_signal<=9'bxxxxxxxxx;
+6'b000000:control_signal<=7'b0001100;// R-type
+6'b100011:control_signal<=7'b1010100;//LW
+6'b101011:control_signal<=7'b0110000;//SW
+6'b000100:control_signal<=7'b0000010;//BEQ
+6'b001000:control_signal<=7'b0010100;//ADDI
+6'b000010:control_signal<=7'b0000001;//J
+default:control_signal<=7'bxxxxxxx;
 endcase
 endmodule
