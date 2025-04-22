58bc5d51122531482dde3226ed173204cbb6a761
KEN-MOITRA <ken.moitra@gmail.com>
2025-04-22 16:40:51 +0530

    Data_memory.v

--- Data_memory.v
+++ Data_memory.v
@@ -1,25 +1,22 @@
-`timescale 1ns / 1ps
-
-module Data_memory
-(
+module Data_memory(
 input clk,
 input rst,
 input mem_write,
-input[31:0] alu_output,
-input[31:0] write_data,
-output[31:0] read_data
+input [31:0] alu_output,
+input [31:0] write_data,
+output [31:0] read_data
 );
 
-reg[31:0] data_memory[0:127];
+reg [31:0] data_memory[0:127];
 
 initial 
-$readmemh("data_for_datamem.txt",data_memory);
+$readmemh("data_for_datamem.txt", data_memory);
 
-assign read_data = (~rst) ? data_memory[alu_output]:32'h00000000;
+assign read_data = (rst) ? 32'h00000000 : data_memory[alu_output];
 
 always@(posedge clk)
 begin
-if(mem_write) data_memory[alu_output] <= write_data;
+    if (!rst && mem_write) 
+        data_memory[alu_output] <= write_data;
 end
 endmodule
-
